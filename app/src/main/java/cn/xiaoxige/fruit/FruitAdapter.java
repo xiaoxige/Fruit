f4bbdc963149c83c71611a9cefb5172049de2c89
zhuxiaoan <zhuxiaoan@pangpangpig.com>
2018-07-25 15:51:57 +0800

    master upldate 重复点击事件为排除

--- app/src/main/java/cn/xiaoxige/fruit/FruitAdapter.java
+++ app/src/main/java/cn/xiaoxige/fruit/FruitAdapter.java
@@ -1,7 +1,6 @@
 package cn.xiaoxige.fruit;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
